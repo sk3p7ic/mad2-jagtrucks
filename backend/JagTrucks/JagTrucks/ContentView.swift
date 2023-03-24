b1c0fa408fe93ea018140b9cd8f8cc54f561d40d
Joshua Ibrom <joshuaibrom@gmail.com>
2023-03-24 08:07:51 -0500

    Created a new XCode SwiftUI project for the frontend

---
+++ backend/JagTrucks/JagTrucks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JagTrucks
+//
+//  Created by Joshua Ibrom on 3/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("JagTrucks")
+                .font(.largeTitle)
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ backend/JagTrucks/JagTrucks/JagTrucksApp.swift
@@ -0,0 +1,17 @@
+//
+//  JagTrucksApp.swift
+//  JagTrucks
+//
+//  Created by Joshua Ibrom on 3/24/23.
+//
+
+import SwiftUI
+
+@main
+struct JagTrucksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
