cacb3f474fd8c37b8cec9e9f4414b7c4579a3822
Joshua Ibrom <joshuaibrom@gmail.com>
2023-04-22 22:11:46 -0500

    Did some work making new views and adding a bottom menu

--- frontend/JagTrucks/JagTrucks/views/ContentView.swift
+++ frontend/JagTrucks/JagTrucks/views/ContentView.swift
@@ -7,12 +7,31 @@
 
 import SwiftUI
 
+private struct HomeView: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello world!")
+                .navigationTitle("Home")
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("JagTrucks")
+        TabView {
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+            HomeView()
+                .tabItem {
+                    Label("Schedule", systemImage: "calendar")
+                }
+            FoodTruckListView()
+                .tabItem {
+                    Label("Trucks", systemImage: "box.truck")
+                }
         }
-        .padding()
     }
 }
 
